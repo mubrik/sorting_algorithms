95e1c385f4d6f59c5f3469ba60e1de34ad3b975a
mubrik <mubarakg4u@gmail.com>
2023-01-22 11:47:52 +0100

    sect fix 1

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,6 +9,38 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i = 0, j = 0, pos = 0;
+	int sel_num, tmp;
+
+	if (size < 2)
+		return;
+
+	while (array && i <= (size - 1))
+	{
+		sel_num = array[i], j = i + 1, pos = i;
+		while (j <= (size - 1))
+		{
+			if (array[j] < sel_num)
+				sel_num = array[j], pos = j;
+			j++;
+		}
+		if (pos != i)
+		{
+			tmp = array[i], array[i] = sel_num, array[pos] = tmp;
+			print_array(array, size);
+		}
+		i++;
+	}
+}
+
+/**
+ * selection_sort - selection_sort original
+ * @array: array to sort
+ * @size: size of array to sort
+ * Return: ptr to arr or null
+ */
+void selection_sort_b(int *array, size_t size)
+{
 	size_t i = 0, j = 0;
 	int sel_num, tmp;
 
