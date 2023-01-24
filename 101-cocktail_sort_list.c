0f710cd517a10817a6d468f3c0ad590df640ff47
mubrik <mubarakg4u@gmail.com>
2023-01-24 02:22:38 +0100

    gaddamn betty

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -5,6 +5,7 @@
  * swap_l - swap_l
  * @first_n: the node that comes first in the list
  * @second_n: the node that comes after
+ * @list: list to sort
  * Return: void
  */
 void swap_l(listint_t *first_n, listint_t *second_n, listint_t **list)
@@ -30,72 +31,96 @@ void swap_l(listint_t *first_n, listint_t *second_n, listint_t **list)
 }
 
 /**
+ * foward_betty - foward_betty
+ * @list: list
+ * @head: head
+ * @st_in: ptr to start index
+ * @swp: ptr to swap
+ * @end_in: end index
+ * Return: listint_t
+ * Description: Betty can be so annoying
+ */
+listint_t *foward_betty(listint_t **list, listint_t *head, int *st_in,
+	int *swp, int end_in)
+{
+	listint_t *tmp = NULL;
+	int i = 0;
+	/*foward iter*/
+	for (i = 0; head && (i <= end_in); head = head->next, i++)
+	{
+		/* start at start index */
+		if (i < *st_in)
+			continue;
+		if (head->next) /* compare */
+		{
+			if (head->n > head->next->n)
+				tmp = head->next, swap_l(head, head->next, list), *swp = 1, head = tmp;
+		}
+		else
+		{
+			*st_in = *st_in + 1;
+			break;
+		}
+	}
+	return (head);
+}
+
+/**
+ * backward_betty - backward_betty
+ * @list: list
+ * @head: head
+ * @st_in: start index
+ * @swp: ptr to swap
+ * @end_in: ptr to end index
+ * Return: void
+ * Description: Betty can be so annoying
+ */
+listint_t *backward_betty(listint_t **list, listint_t *head, int st_in,
+	int *swp, int *end_in)
+{
+	listint_t *tmp = NULL;
+	int j = 0;
+	/* backward iter */
+	for (j = *end_in; head && (j >= st_in); head = head->prev, j--)
+	{
+		if (head->prev) /* compare */
+		{
+			if (head->n < head->prev->n)
+				tmp = head->prev, swap_l(head->prev, head, list), *swp = 1, head = tmp;
+		}
+		else
+		{
+			*end_in = *end_in + 1;
+			break;
+		}
+	}
+	return (head);
+}
+
+/**
  * cocktail_sort_list - cocktail_sort_list
  * @list: list to sort
  * Return: void
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *head = NULL, *tmp = NULL;
-	int swap = 0, i = 0, j = 0, st_in = 0, len = 0, end_in = 0;
+	listint_t *head = NULL;
+	int swap = 0, st_in = 0, len = 0, end_in = 0;
 
 	if (!list)
 		return;
-	/* get lst length */
 	head = *list;
-	while (head)
-		len++, head = head->next;
-	/* assign index */
-	st_in = 0, end_in = len - 1;
-	/* quick check */
+	while (head) /* get lst length & assign index */
+		len++, head = head->next, end_in = len - 1;
 	if (len < 2)
 		return;
-	head = *list;
-	/* main loop to hold both iterations */
-	do
-	{
-		/* make sure swap is false on each iteration */
-		swap = 0;
-		/* aways start from head */
-		head = *list;
-		/* iteration going foward */
-		for (i = 0; head && (i <= end_in); head = head->next, i++)
-		{
-			/* start at stin */
-			if (i < st_in)
-				continue;
-			/* compare */
-			if (head->next)
-			{
-				if (head->n > head->next->n)
-					tmp = head->next, swap_l(head, head->next, list), swap = 1, head = tmp;
-			}
-			else
-			{
-				st_in++;
-				break;
-			}
-		}
-		/* break main loop if no swap */
-		if (!swap)
+	do {	/* main loop to hold both iterations */
+		swap = 0, head = *list;	/* make sure swap is false on each iteration */
+		head = foward_betty(list, head, &st_in, &swap, end_in);
+		if (!swap) /* break main loop if no swap */
 			break;
-		/* start index -1 */
+		/* start index - 1 */
 		head = head->prev;
-		/* iteration going backwards */
-		for (j = end_in; head && (j >= st_in); head = head->prev, j--)
-		{
-			/* compare */
-			if (head->prev)
-			{
-				if (head->n < head->prev->n)
-					tmp = head->prev, swap_l(head->prev, head, list), swap = 1, head = tmp;
-			}
-			else
-			{
-				end_in--;
-				break;
-			}
-		}
+		head = backward_betty(list, head, st_in, &swap, &end_in);
 	} while (swap);
-
 }
