3bce3fd242e42c47d062acbb989d5be383c1a4fb
mubrik <mubarakg4u@gmail.com>
2023-01-24 18:58:34 +0100

    counting betyy

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -12,29 +12,24 @@ void counting_sort(int *array, size_t size)
 {
 	int max_n = 0, *count_l = NULL, *output = NULL, key = 0, pos = 0;
 	size_t i = 0;
-
 	/* quick check */
 	if (size < 2)
 		return;
-	/* get max size in array */
-	while (i < size)
+	while (i < size)	/* get max size in array */
 	{
 		if (array[i] > max_n)
 			max_n = array[i];
 		i++;
 	}
-	/* accomodation for the max num itself */
-	max_n++;
+	max_n++; /* accomodating for the max num itself */
 	/* alloc */
-	count_l = malloc(sizeof(int) * max_n);
-	output = malloc(sizeof(int) * size);
+	count_l = malloc(sizeof(int) * max_n), output = malloc(sizeof(int) * size);
 	if (!count_l || !output)
 	{
 		free(count_l), free(output);
 		return;
 	}
-	i = 0;
-	/* init both to be 0's */
+	i = 0;	/* init both to be 0's */
 	while (i < (size_t) max_n || i < size)
 	{
 		if (i < size)
@@ -43,24 +38,15 @@ void counting_sort(int *array, size_t size)
 			count_l[i] = 0;
 		i++;
 	}
-	/* start counting */
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)	/* start counting */
 		key = array[i], count_l[key] = count_l[key] + 1;
-	/* sum computation */
-	for (i = 1; i < (size_t) max_n; i++)
+	for (i = 1; i < (size_t) max_n; i++)	/* sum computation */
 		count_l[i] = count_l[i] + count_l[i - 1];
-	/* debug print count */
-	for (i = 0; i < (size_t) max_n; i++)
+	for (i = 0; i < (size_t) max_n; i++)	/* debug print count */
 		printf("%d%s", count_l[i], i == (size_t) (max_n - 1) ? "\n" : ", ");
-	/* place number in output by using count and decrementing */
-	for (i = 0; i < size; i++)
-	{
-		key = array[i], pos = count_l[key] = count_l[key] - 1;
-		output[pos] = key;
-	}
-	/* modify original */
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)	/* sort in output by using count and decr-- */
+		key = array[i], pos = count_l[key] = count_l[key] - 1, output[pos] = key;
+	for (i = 0; i < size; i++)	/* modify original */
 		array[i] = output[i];
-	/* free */
-	free(count_l), free(output);
+	free(count_l), free(output);	/* free */
 }
